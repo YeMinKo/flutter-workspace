749e2d86cc7ca0029052247d569e4e9a07e9f5ca
Ye Min Ko <yeminko47@gmail.com>
2022-12-27 07:29:33 +0630

    [added] product save feature

--- shop_app/lib/screens/edit_product_screen.dart
+++ shop_app/lib/screens/edit_product_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop_app/providers/product.dart';
+import 'package:shop_app/providers/products.dart';
 
 class EditProductScreen extends StatefulWidget {
   static const routeName = '/edit-product';
@@ -45,6 +47,8 @@ class _EditProductScreenState extends State<EditProductScreen> {
     }
 
     _form.currentState.save();
+    Provider.of<Products>(context, listen: false).addProduct(_editedProduct);
+    Navigator.of(context).pop();
     print(_editedProduct.id);
     print(_editedProduct.title);
     print(_editedProduct.price);
