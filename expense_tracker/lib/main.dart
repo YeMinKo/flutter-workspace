f6cef83afb89f893a91de8466d30f01e19231f72
Ye Min Ko <yeminko47@gmail.com>
2023-06-05 09:37:11 +0630

    [added] more ui and AppBar

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -1,6 +1,10 @@
-import 'package:expense_tracker/expenses.dart';
+import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Expenses());
+  runApp(
+    const MaterialApp(
+      home: Expenses(),
+    ),
+  );
 }
